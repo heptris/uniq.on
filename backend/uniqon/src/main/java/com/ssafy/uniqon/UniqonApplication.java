4aa0c8c92ee45903431dae4cdc990f9668b6889d
HojinAn <asdv94@naver.com>
2022-09-05 12:56:51 +0900

    :tada: [ADD] BackEnd 초기 세팅

---
+++ backend/uniqon/src/main/java/com/ssafy/uniqon/UniqonApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.uniqon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqonApplication.class, args);
+	}
+
+}
