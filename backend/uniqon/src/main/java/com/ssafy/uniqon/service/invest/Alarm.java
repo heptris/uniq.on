6baf71d5699cd6f2f35d34c0177cdab3f2d5861c
dltmdgh579 <dltmdgh579@gmail.com>
2022-09-06 11:59:01 +0900

    :package: [ADD] package 구조 생성

---
+++ backend/uniqon/src/main/java/com/ssafy/uniqon/service/invest/Alarm.java
@@ -0,0 +1,27 @@
+package com.ssafy.uniqon.service.invest;
+
+import com.ssafy.uniqon.domain.Member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Entity
+public class Alarm {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JoinColumn(name = "alarm_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String content;
+
+    private boolean isRead;
+}
