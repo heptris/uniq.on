3d0b64cb31a5e3352130eca41e8e21b28bb6f3a9
YanZisuka <hanndrednine@gmail.com>
2022-09-14 15:27:26 +0900

    :alembic: [ADD] 컴포넌트 추상화를 위한 유틸타입 & 추상화된 컴포넌트 제안

---
+++ frontend/components/Text/index.tsx
@@ -0,0 +1,14 @@
+import { ElementType, Ref, forwardRef } from "react";
+import { TextProps } from "@/types/index";
+
+function Text<T extends ElementType = "span">(
+  { typography = "content", as, ...props }: TextProps<T>,
+  ref: Ref<any>
+) {
+  const target = as ?? "span";
+  const Component = target;
+
+  return <Component ref={ref} {...props} />;
+}
+
+export default forwardRef(Text) as typeof Text;
