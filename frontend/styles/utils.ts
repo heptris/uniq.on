1dccc69d36abab655ecda8ecc276e31c7af2bcbf
YanZisuka <hanndrednine@gmail.com>
2022-09-16 00:43:40 +0900

    :lipstick: [FIX] convex util css variable 적용으로 유연성 증가

--- frontend/styles/utils.ts
+++ frontend/styles/utils.ts
@@ -1,5 +1,5 @@
 /* 반복적으로 사용될 만한 유틸 스타일 */
 
 export const convex = `
-  box-shadow: inset -1px 1px 2px #7387ba, inset 1px -1px 2px #444;
+  box-shadow: inset -1px 1px 2px var(--whiteOpacity600), inset 1px -1px 2px var(--greyOpacity800);
 `;
