615a5f132ef34aff0902def08101e5060ed920cc
박상태 <sangtae365@naver.com>
2022-09-15 14:49:03 +0900

    :sparkles: [FEAT] Member 수정 삭제, MemberType USER로 통합[S07P22A507-36]
    Member 수정 삭제, MemberType USER로 통합

--- backend/uniqon/src/main/java/com/ssafy/uniqon/controller/member/MemberController.java
+++ backend/uniqon/src/main/java/com/ssafy/uniqon/controller/member/MemberController.java
@@ -1,19 +1,54 @@
 package com.ssafy.uniqon.controller.member;
 
+import com.ssafy.uniqon.dto.member.MemberUpdateDto;
 import com.ssafy.uniqon.dto.response.ResponseDto;
+import com.ssafy.uniqon.exception.ex.CustomException;
+import com.ssafy.uniqon.exception.ex.CustomValidationException;
+import com.ssafy.uniqon.service.member.MemberService;
+import com.ssafy.uniqon.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.ssafy.uniqon.exception.ex.ErrorCode.NOT_EQUAL_PASSWORD;
 
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/api/member")
 public class MemberController {
 
-    @GetMapping
-    public ResponseEntity test() {
-        return new ResponseEntity<ResponseDto>(new ResponseDto(200, "success", null)
-        , HttpStatus.OK);
+    private final MemberService memberService;
+
+    @PutMapping
+    public ResponseEntity memberUpdate(@RequestBody @Valid MemberUpdateDto memberUpdateDto, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            throw new CustomValidationException("유효성 검사 실패", errorMap);
+        } else {
+            Long memberId = SecurityUtil.getCurrentMemberId();
+            memberService.memberUpdate(memberId, memberUpdateDto);
+        }
+        return new ResponseEntity(new ResponseDto<>(200,"success","수정 완료 !!"), HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity memberDelete(){
+        Long memberId = SecurityUtil.getCurrentMemberId();
+        memberService.memberDelete(memberId);
+
+        return new ResponseEntity(new ResponseDto<>(200,"success","삭제 완료 !!"), HttpStatus.OK);
     }
 }
--- backend/uniqon/src/main/java/com/ssafy/uniqon/service/member/MemberService.java
+++ backend/uniqon/src/main/java/com/ssafy/uniqon/service/member/MemberService.java
@@ -1,10 +1,16 @@
 package com.ssafy.uniqon.service.member;
 
 import com.ssafy.uniqon.domain.member.Member;
+import com.ssafy.uniqon.dto.member.MemberUpdateDto;
+import com.ssafy.uniqon.exception.ex.CustomException;
+import com.ssafy.uniqon.exception.ex.ErrorCode;
 import com.ssafy.uniqon.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.ssafy.uniqon.exception.ex.ErrorCode.MEMBER_NOT_FOUND;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -12,9 +18,24 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public boolean existsByNickname(String nickName){
         return memberRepository.existsByNickname(nickName);
     }
+
+    @Transactional
+    public void memberUpdate(Long memberId, MemberUpdateDto memberUpdateDto){
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+
+        member.updateMember(passwordEncoder.encode(memberUpdateDto.getPassword()), memberUpdateDto.getNickName());
+    }
+
+    @Transactional
+    public void memberDelete(Long memberId){
+        memberRepository.deleteById(memberId);
+    }
+
 }
