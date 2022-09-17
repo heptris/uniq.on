9e46d436ad088ea8ea5fe04e1f30ccb630e4d80a
YanZisuka <hanndrednine@gmail.com>
2022-09-18 00:55:57 +0900

    :white_check_mark: [ADD] theme.ts fixture

---
+++ frontend/__fixtures__/theme.ts
@@ -0,0 +1,14 @@
+export const darkTheme = {
+  colors: {
+    pageBgColor: "#171a1f",
+    cardItemBgColor: "#262932",
+    lineItemBgColor: "#353647",
+    txtMainColor: "#fff",
+    txtSubColor: "#5e6272",
+    mainColor: "#a64ce6",
+    emphasisColor: "#2258ed",
+    successColor: "#84cd70",
+    failColor: "#e22e71",
+    disabledColor: "#7b7f90",
+  },
+};
