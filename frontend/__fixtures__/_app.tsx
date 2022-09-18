00b3a35b57f87a177ccd8bf7bedb2b75d8fa75bc
YanZisuka <hanndrednine@gmail.com>
2022-09-19 01:47:55 +0900

    :white_check_mark: [TEST] fixtures Theme 객체 구조 반영

--- frontend/__fixtures__/_app.tsx
+++ frontend/__fixtures__/_app.tsx
@@ -1,12 +1,12 @@
 import { Theme, ThemeProvider, ThemeProviderProps } from "@emotion/react";
-import { darkTheme } from "@/styles/theme";
+import { uniqonThemes } from "@/styles/theme";
 
 import { Combine } from "@/types/utils";
 
 type MyAppProps = Combine<{ theme?: Theme }, ThemeProviderProps>;
 
 function MyApp(props: MyAppProps) {
-  return <ThemeProvider theme={darkTheme} {...props} />;
+  return <ThemeProvider theme={uniqonThemes.darkTheme} {...props} />;
 }
 
 export default MyApp;
--- frontend/__fixtures__/theme.ts
+++ frontend/__fixtures__/theme.ts
@@ -1,14 +1,23 @@
-export const darkTheme = {
-  colors: {
-    pageBgColor: "#171a1f",
-    cardItemBgColor: "#262932",
-    lineItemBgColor: "#353647",
-    txtMainColor: "#fff",
-    txtSubColor: "#5e6272",
-    mainColor: "#a64ce6",
-    emphasisColor: "#2258ed",
-    successColor: "#84cd70",
-    failColor: "#e22e71",
-    disabledColor: "#7b7f90",
+import { UniqonTheme } from "@/styles/theme";
+
+export const darkTheme: UniqonTheme = {
+  name: "DarkTheme",
+  color: {
+    background: {
+      main: "#a64ce6",
+      emphasis: "#2258ed",
+      page: "#171a1f",
+      card: "#262932",
+      item: "#353647",
+    },
+    text: {
+      main: "#ffffff",
+      sub: "#5e6272",
+    },
+    status: {
+      success: "#84cd70",
+      fail: "#e22e71",
+      disabled: "#7b7f90",
+    },
   },
 };
