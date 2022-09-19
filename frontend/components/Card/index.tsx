8b409b73462bb8f0f18ee6592e959cbe04fb87aa
HAN SEUNG JAE <hanndrednine@gmail.com>
2022-09-19 18:01:30 +0900

    :sparkles: [FEAT] commons::Card (진행중)

--- frontend/components/Card/index.tsx
+++ frontend/components/Card/index.tsx
@@ -1,5 +1,12 @@
 import { ElementType, forwardRef, Ref } from "react";
+import Image from "next/image";
 import { CardProps } from "@/types/props";
+
+import { useTheme } from "@emotion/react";
+import { css } from "@emotion/css";
+import { cssConvex } from "@/styles/utils";
+
+import Text from "@/components/Text";
 
 /**
  * @params
@@ -12,7 +19,34 @@ function Card<T extends ElementType = "div">(
   const target = as ?? "div";
   const Component = target;
 
-  return <Component ref={ref} {...props} />;
+  const theme = useTheme();
+
+  return (
+    <Component
+      className={css`
+        width: 100%;
+        height: 10rem;
+        border-radius: 8px;
+        padding: 0 2rem;
+        background-color: ${theme.color.background.card};
+
+        ${cssConvex}
+      `}
+      ref={ref}
+      {...props}
+    >
+      <Text as="h2" role="name">
+        test
+      </Text>
+      <Text as="h1" role="title">
+        Title test
+      </Text>
+      <Text as="h3" role="date">
+        2022.09.07
+      </Text>
+      <Image src="" />
+    </Component>
+  );
 }
 
 export default forwardRef(Card) as typeof Card;
