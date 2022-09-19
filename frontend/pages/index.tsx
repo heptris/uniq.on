2fc69c083b1799359b44aebd1cc3464fcaac77b6
SSAFY <ejshin0213@naver.com>
2022-09-19 14:32:11 +0900

    :lipstick: [DESIGN] home 컴포넌트에 navbar 컴포넌트 적용
     home 컴포넌트에 navbar 컴포넌트 적용

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -7,58 +7,61 @@ import Button from "@/components/Button";
 import Text from "@/components/Text";
 import Container from "@/components/Container";
 import Grid from "@/components/Grid";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <Container>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Navbar>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <Text as="h1" className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </Text>
+        <main>
+          <Text as="h1" className={styles.title}>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </Text>
 
-        <Text as="p" className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </Text>
-        <Button type={"blue"}>상세 페이지 이동</Button>
+          <Text as="p" className={styles.description}>
+            Get started by editing <code>pages/index.js</code>
+          </Text>
+          <Button type={"blue"}>상세 페이지 이동</Button>
 
-        <Grid>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <Text as="h3">Documentation &rarr;</Text>
-            <Text as="p">
-              Find in-depth information about Next.js features and API.
-            </Text>
-          </a>
+          <Grid>
+            <a href="https://nextjs.org/docs" className={styles.card}>
+              <Text as="h3">Documentation &rarr;</Text>
+              <Text as="p">
+                Find in-depth information about Next.js features and API.
+              </Text>
+            </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <Text as="h3">Learn &rarr;</Text>
-            <Text as="p">
-              Learn about Next.js in an interactive course with quizzes!
-            </Text>
-          </a>
+            <a href="https://nextjs.org/learn" className={styles.card}>
+              <Text as="h3">Learn &rarr;</Text>
+              <Text as="p">
+                Learn about Next.js in an interactive course with quizzes!
+              </Text>
+            </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <Text as="h3">Examples &rarr;</Text>
-            <Text as="p">
-              Discover and deploy boilerplate example Next.js projects.
-            </Text>
-          </a>
+            <a
+              href="https://github.com/vercel/next.js/tree/canary/examples"
+              className={styles.card}
+            >
+              <Text as="h3">Examples &rarr;</Text>
+              <Text as="p">
+                Discover and deploy boilerplate example Next.js projects.
+              </Text>
+            </a>
 
-          <a href="https://vercel.com/new" className={styles.card}>
-            <Text as="h3">Deploy &rarr;</Text>
-            <Text as="p">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </Text>
-          </a>
-        </Grid>
-      </main>
+            <a href="https://vercel.com/new" className={styles.card}>
+              <Text as="h3">Deploy &rarr;</Text>
+              <Text as="p">
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </Text>
+            </a>
+          </Grid>
+        </main>
+      </Navbar>
 
       <footer className={styles.footer}>
         <a
