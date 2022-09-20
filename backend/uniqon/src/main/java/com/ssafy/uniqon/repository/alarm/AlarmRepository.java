ab38f267ba8228127301df63b96a7e544f646962
전한울 <wjsgksdnf11@naver.com>
2022-09-20 11:33:31 +0900

    :recycle: [REFACTOR] AlarmRepository 경로 수정
    ALarmRepository 경로 수정

--- backend/uniqon/src/main/java/com/ssafy/uniqon/repository/alarm/AlarmRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.uniqon.repository.alarm;
-
-import com.ssafy.uniqon.domain.alarm.Alarm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlarmRepository extends JpaRepository<Alarm, Long> {
-}
