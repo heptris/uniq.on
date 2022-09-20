237ad0f854550218893271b729241006cd67c2ea
YanZisuka <hanndrednine@gmail.com>
2022-09-20 17:56:52 +0900

    :test_tube: [TEST] test::Card

--- frontend/__tests__/card.test.tsx
+++ frontend/__tests__/card.test.tsx
@@ -1,7 +1,12 @@
-import { getByRole, getByTestId, render } from "@testing-library/react";
+import { getByTestId, render } from "@testing-library/react";
 
 import MyApp from "../__fixtures__/_app";
 import Card from "@/components/Card";
+import CorporationCard from "@/components/Card/CorporationCard";
+import NFTItemCard from "@/components/Card/NFTItemCard";
+
+import nft from "@/assets/nfts/1.png";
+import corp from "@/assets/corps/1.png";
 
 describe("Card", () => {
   const { container } = render(
@@ -10,24 +15,50 @@ describe("Card", () => {
     </MyApp>
   );
   const card = getByTestId(container, "card");
-  const name = getByRole(container, "name");
-  const title = getByRole(container, "title");
-  const date = getByRole(container, "date");
-  const nftImage = getByRole(container, "nftImage");
 
   it("renders a card", () => {
     expect(card).toBeInTheDocument();
   });
-  it("renders a card with name", () => {
-    expect(name).toBeInTheDocument();
+  it("renders a CorporationCard", () => {
+    const { container } = render(
+      <MyApp>
+        <CorporationCard
+          data-testid="corporation-card"
+          corpName={"Tester"}
+          corpAvatar={corp}
+          title={"testTitle"}
+          date={"testDate"}
+          progress={38}
+        />
+      </MyApp>
+    );
+    const corporationCard = getByTestId(container, "corporation-card");
+
+    expect(corporationCard).toBeInTheDocument();
+    expect(corporationCard).toHaveTextContent("Tester");
+    expect(corporationCard).toHaveTextContent("testTitle");
+    expect(corporationCard).toHaveTextContent("testDate");
   });
-  it("renders a card with title", () => {
-    expect(title).toBeInTheDocument();
-  });
-  it("renders a card with date", () => {
-    expect(date).toBeInTheDocument();
-  });
-  it("renders a card with NFT", () => {
-    expect(nftImage).toBeInTheDocument();
+  it("renders a NFTItemCard", () => {
+    const { container } = render(
+      <MyApp>
+        <NFTItemCard
+          data-testid="nft-item-card"
+          nftImage={nft}
+          tokenName={"testTitle #199"}
+          corpName={"Tester"}
+          price={1}
+          status={"망함"}
+          progress={38}
+        />
+      </MyApp>
+    );
+    const nftItemCard = getByTestId(container, "nft-item-card");
+
+    expect(nftItemCard).toBeInTheDocument();
+    expect(nftItemCard).toHaveTextContent("testTitle");
+    expect(nftItemCard).toHaveTextContent("Tester");
+    expect(nftItemCard).toHaveTextContent("망함");
+    expect(nftItemCard).toHaveTextContent("1");
   });
 });
