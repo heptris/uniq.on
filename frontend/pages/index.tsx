f72121295eca928c3737e304539336b045358b29
신은정 <ejshin0213@naver.com>
2022-09-22 03:10:36 +0900

    :sparkles: [FEAT] home page에 carousel 생성
    home page에 carousel 생성

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -9,56 +9,157 @@ import Navbar from "@/components/Navbar";
 import Card from "@/components/Card";
 import SelectTab from "@/components/SelectTab";
 import Footer from "@/components/Footer";
+import { css } from "@emotion/css";
+import { useTheme } from "@emotion/react";
+import { useEffect, useRef, useState } from "react";
+import mainimg1 from "assets/mainimg1.jpeg";
+import mainimg2 from "assets/mainimg2.jpeg";
+import mainimg3 from "assets/mainimg3.jpeg";
 
 export default function Home() {
+  const theme = useTheme();
+  const images = useRef([
+    {
+      src: mainimg1,
+    },
+    {
+      src: mainimg2,
+    },
+    {
+      src: mainimg3,
+    },
+  ]);
+
+  const [current, setCurrent] = useState(0);
+  const [style, setStyle] = useState({
+    marginLeft: `-${current}00%`,
+  });
+  const imgSize = useRef(images.current.length);
+
+  const moveSlide = (i: number) => {
+    let nextIndex = current + i;
+
+    if (nextIndex < 0) nextIndex = imgSize.current - 1;
+    else if (nextIndex >= imgSize.current) nextIndex = 0;
+
+    setCurrent(nextIndex);
+  };
+
+  useEffect(() => {
+    setStyle({ marginLeft: `-${current}00%` });
+  }, [current]);
+
   return (
     <Container>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Navbar />
+      <div
+        className={css`
+          width: 100%;
+          padding: 2rem 0;
+          flex: 1 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          align-items: center;
+          color: ${theme.color.text.main};
 
-      <main>
-        <Navbar />
-        <Text as="h1">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </Text>
-        <Text as="p">
-          Get started by editing <code>pages/index.js</code>
-        </Text>
-        <Button type={"blue"}>상세 페이지 이동</Button>
+          .slide {
+            width: 100%;
+            display: flex;
+            align-items: center;
+          }
 
-        <Grid>
-          <Card />
-          <a href="https://nextjs.org/docs">
-            <Text as="h3">Documentation &rarr;</Text>
-            <Text as="p">
-              Find in-depth information about Next.js features and API.
-            </Text>
-          </a>
+          .window {
+            background: coral;
+            width: 315px;
+            height: 215px;
+            overflow: hidden;
+          }
 
-          <a href="https://nextjs.org/learn">
-            <Text as="h3">Learn &rarr;</Text>
-            <Text as="p">
-              Learn about Next.js in an interactive course with quizzes!
-            </Text>
-          </a>
+          .flexbox {
+            display: flex;
+          }
 
-          <a href="https://github.com/vercel/next.js/tree/canary/examples">
-            <Text as="h3">Examples &rarr;</Text>
-            <Text as="p">
-              Discover and deploy boilerplate example Next.js projects.
-            </Text>
-          </a>
+          .img {
+            width: 315px;
+            height: 215px;
+            background-position: 50% 50%;
+            background-size: contain;
+            background-repeat: no-repeat;
+            flex: none;
+          }
 
-          <a href="https://vercel.com/new">
-            <Text as="h3">Deploy &rarr;</Text>
-            <Text as="p">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </Text>
-          </a>
-        </Grid>
-      </main>
+          .btn {
+            display: flex;
+            align-items: center;
+            cursor: pointer;
+            font-size: 2rem;
+            color: gray;
+            padding: 0 10px;
+          }
+
+          .position {
+            margin-top: 15px;
+            display: flex;
+            justify-content: center;
+          }
+
+          .dot {
+            background: ${theme.color.status.disabled};
+            border-radius: 100%;
+            height: 10px;
+            width: 10px;
+          }
+          .dot + .dot {
+            margin-left: 20px;
+          }
+
+          .current {
+            background: ${theme.color.text.sub};
+          }
+        `}
+      >
+        <div className="slide">
+          <div
+            className="btn"
+            onClick={() => {
+              moveSlide(-1);
+            }}
+          >
+            &lt;
+          </div>
+          <div className="window">
+            <div className="flexbox" style={style}>
+              {images.current.map((img, i) => (
+                <div className="img">
+                  <Image
+                    src={img.src}
+                    alt="img"
+                    width={315}
+                    height={215}
+                  ></Image>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div
+            className="btn"
+            onClick={() => {
+              moveSlide(1);
+            }}
+          >
+            &gt;
+          </div>
+        </div>
+        <div className="position">
+          {images.current.map((x, i) => (
+            <div
+              key={i}
+              className={i === current ? "dot current" : "dot"}
+            ></div>
+          ))}
+        </div>
+      </div>
 
       <Footer />
     </Container>
