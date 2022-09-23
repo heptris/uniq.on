38870d567e13f9e38129d6b3cdb86838549bb0e2
YanZisuka <hanndrednine@gmail.com>
2022-09-23 15:25:13 +0900

    :art: [FIX] Layout::스타일 정리

--- frontend/components/Layout/index.tsx
+++ frontend/components/Layout/index.tsx
@@ -6,25 +6,9 @@ import { useTheme, css } from "@emotion/react";
 export default function Layout({ children }: { children: React.ReactNode }) {
   const theme = useTheme();
   return (
-    <Container
-      css={css`
-        padding: 0;
-      `}
-    >
+    <Container>
       <Navbar />
-      <div
-        css={css`
-          width: 100%;
-          flex: 1 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: flex-start;
-          align-items: center;
-          color: ${theme.color.text.main};
-        `}
-      >
-        {children}
-      </div>
+      {children}
       <Footer />
     </Container>
   );
