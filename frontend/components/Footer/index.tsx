379c5a78aa72e2aa7bd94dbfdaee1abaa324ce19
YanZisuka <hanndrednine@gmail.com>
2022-09-23 15:22:19 +0900

    :lipstick: [DESIGN] Footer

--- frontend/components/Footer/index.tsx
+++ frontend/components/Footer/index.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { css, useTheme } from "@emotion/react";
-
+import styled from "@emotion/styled";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopyright } from "@fortawesome/free-solid-svg-icons";
 
@@ -12,134 +11,135 @@ import Text from "@/components/Text";
  */
 function Footer() {
   const theme = useTheme();
+
   return (
-    <footer
-      css={css`
-        width: 100%;
-        height: 15rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        color: ${theme.color.text.main};
-        .infolist {
-          margin: 0 7rem;
-        }
-        h1 {
-          font-size: 1.3rem;
-          font-weight: bold;
-          margin-bottom: 1rem;
-        }
-        p {
-          font-size: 0.8rem;
-          margin-bottom: 0.5rem;
-        }
-        span {
-          color: ${theme.color.text.sub};
-        }
-        .coinfo {
-          font-size: 0.9rem;
-        }
-        .infolist p {
-          &:hover {
-            color: ${theme.color.text.hover};
-            cursor: pointer;
-          }
-        }
-        @media screen and (max-width: 768px) {
-          height: 18rem;
-          .infolist {
-            margin: 0 1.3rem;
-          }
-          .coinfo {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            line-height: 130%;
-          }
-          .slash {
-            visibility: hidden;
-          }
-        }
-      `}
-    >
-      <div
+    <FooterContainer>
+      <Text
+        as="h1"
+        role="logo"
         css={css`
-          width: 100%;
-          display: flex;
-          flex-direction: row;
-          justify-content: center;
-          align-items: flex-start;
-          margin: 1rem 0 2rem;
+          font-size: 2rem;
+          font-weight: 700;
         `}
       >
-        <div className="infolist">
-          <h1>Invest</h1>
-          <p>투자리스트</p>
-          <p>투자신청</p>
-        </div>
-        <div className="infolist">
-          <h1>My account</h1>
-          <p>마이페이지</p>
-        </div>
-        <div className="infolist">
-          <h1>Company</h1>
-          <p>자주하는 질문</p>
-        </div>
-      </div>
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          margin-bottom: 1rem;
-        `}
-      >
-        <div className="coinfo">
-          <span
-            css={css`
-              font-size: 1.5rem;
-              font-weight: 700;
-              margin-bottom: 2rem;
-            `}
-          >
-            Uniq.on <span className="slash">|</span>
-          </span>
-          <span>
-            A507 안호진 박상태 신은정 이승호 전한울 한승재{" "}
-            <span className="slash">|</span>
-          </span>
-          <span>
-            서울특별시 강남구 테헤란로 212 (역삼동){" "}
-            <span className="slash">|</span>
-          </span>
-          <span> Email hanndrednine@gmail.com</span>
-        </div>
-        <div
+        uniq.on
+      </Text>
+
+      <InfoContainer>
+        <InfoItem>
+          <Text as="h1" role="info-header">
+            Investment
+          </Text>
+          <ul>
+            <Text as="li">투자리스트</Text>
+            <Text as="li">투자신청</Text>
+          </ul>
+        </InfoItem>
+        <InfoItem>
+          <Text as="h1" role="info-header">
+            My account
+          </Text>
+          <ul>
+            <Text as="li">마이페이지</Text>
+          </ul>
+        </InfoItem>
+        <InfoItem>
+          <Text as="h1" role="info-header">
+            Company
+          </Text>
+          <ul>
+            <Text as="li">자주하는 질문</Text>
+          </ul>
+        </InfoItem>
+      </InfoContainer>
+
+      <Developers>
+        <Text as="h1">A507</Text>
+        <Text role="developers">안호진 박상태 신은정 이승호 전한울 한승재</Text>
+        <Text as="h2">asdv94@naver.com</Text>
+        <Text as="h3">서울특별시 강남구 테헤란로 212 (역삼동) </Text>
+      </Developers>
+
+      <Copyright>
+        <Text>COPYRIGHT</Text>
+        <FontAwesomeIcon
           css={css`
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            align-items: center;
-            color: ${theme.color.text.sub};
-            margin-top: 1rem;
+            width: 15px;
+            margin: 0 0.2rem 0.5rem;
           `}
-        >
-          <p>COPYRIGHT</p>
-          <FontAwesomeIcon
-            css={css`
-              width: 15px;
-              margin: 0 0.2rem 0.5rem;
-            `}
-            icon={faCopyright}
-          />
-          <p> Uniq.on ALL RIGHTS RESERVED</p>
-        </div>
-      </div>
-    </footer>
+          icon={faCopyright}
+        />
+        <Text>uniq.on ALL RIGHTS RESERVED</Text>
+      </Copyright>
+    </FooterContainer>
   );
 }
+
+const FooterContainer = styled.footer`
+  border: 0;
+  margin: 3rem 0 0 0;
+  width: 100%;
+  height: 50rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.color.text.main};
+  background-color: ${({ theme }) => theme.color.background.card};
+`;
+const InfoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  margin-top: 3rem;
+`;
+const InfoItem = styled.div`
+  h1 {
+    font-size: 1.2rem;
+    font-weight: 500;
+    margin-bottom: 0.5rem;
+  }
+  ul {
+    li {
+      color: ${({ theme }) => theme.color.text.sub};
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+`;
+const Developers = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 5rem;
+
+  h1,
+  span,
+  h2,
+  h3 {
+    color: ${({ theme }) => theme.color.text.sub};
+  }
+  h1 {
+    font-size: 0.8rem;
+  }
+  span {
+    font-size: 0.9rem;
+  }
+  h2,
+  h3 {
+    font-size: 0.8rem;
+  }
+`;
+const Copyright = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 5rem;
+  color: ${({ theme }) => theme.color.text.sub};
+
+  span {
+    color: ${({ theme }) => theme.color.text.sub};
+  }
+`;
 
 export default Footer;
