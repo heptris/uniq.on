cc5d96fbe1c0f2371c193e60a03a53859f77e04a
HojinAn <asdv94@naver.com>
2022-09-23 12:24:40 +0900

    :white_check_mark: [FEAT] 성공 코드를 통한 LabelInput 컴포넌트 제작

---
+++ frontend/components/LabelInput/index.tsx
@@ -0,0 +1,65 @@
+import { css, useTheme } from "@emotion/react";
+import { ElementType, forwardRef, Ref } from "react";
+
+import { LabelInputProps } from "@/types/props";
+
+/**
+ * @params
+ *
+ * @return ReactElement
+ */
+function LabelInput<T extends ElementType = "input">(
+  { as, placeholder, labelText, textValue, ...props }: LabelInputProps<T>,
+  ref: Ref<any>
+) {
+  const target = as ?? "input";
+  const Component = target;
+
+  const { color } = useTheme();
+
+  const content = (
+    <Component
+      ref={ref}
+      css={css`
+        border-radius: 4px;
+        background-color: ${color.background.item};
+        &:focus {
+          outline: 2px solid ${color.border.main};
+        }
+      `}
+      placeholder={placeholder}
+      id={labelText}
+      value={textValue}
+      {...props}
+    />
+  );
+
+  return (
+    <>
+      {labelText !== undefined ? (
+        <div
+          css={css`
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          <label
+            htmlFor={labelText}
+            css={css`
+              color: ${color.text.main};
+              font-size: 12px;
+              margin-bottom: 10px;
+            `}
+          >
+            {labelText}
+          </label>
+          {content}
+        </div>
+      ) : (
+        <>{content}</>
+      )}
+    </>
+  );
+}
+
+export default forwardRef(LabelInput) as typeof LabelInput;
