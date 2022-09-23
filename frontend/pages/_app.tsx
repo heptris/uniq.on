f6d8366d583c03e3a5deb4ca09d5eed064322379
신은정 <ejshin0213@naver.com>
2022-09-23 17:55:33 +0900

    :recycle: [REFACTOR] app.js에서 layout 컴포넌트와 Head 전역적으로 사용하도록 코드수정

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,11 +5,19 @@ import type { AppProps } from "next/app";
 
 import { ThemeProvider } from "@emotion/react";
 import { uniqonThemes } from "@/styles/theme";
+import Layout from "@/components/Layout";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={uniqonThemes.darkTheme}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Uniq.on</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   );
 }
