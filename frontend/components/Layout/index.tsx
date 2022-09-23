754d843c93dea293d525ebe75f015d3cf9e9da41
신은정 <ejshin0213@naver.com>
2022-09-23 18:00:46 +0900

    :coffin: [REMOVE] layout 컴포넌트의 불필요한 코드 삭제

--- frontend/components/Layout/index.tsx
+++ frontend/components/Layout/index.tsx
@@ -1,10 +1,8 @@
 import Container from "../Container";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
-import { useTheme, css } from "@emotion/react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const theme = useTheme();
   return (
     <Container>
       <Navbar />
