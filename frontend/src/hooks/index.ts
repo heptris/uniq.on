e05f9e3269e1539f9116479ae4d255e097b190b6
HojinAn <asdv94@naver.com>
2022-09-27 16:30:23 +0900

    :sparkles: [FEAT] 전역 로그인 상태 관리 위해 Redux 세팅

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,2 +1,11 @@
 export * from "@/hooks/useModal";
 export * from "@/hooks/useInterval";
+export * from "@/hooks/useAuth";
+
+import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+import type { RootState, AppDispatch } from "@/store";
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
