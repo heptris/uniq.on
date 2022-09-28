7e718ccef5dde9b5dc5224b2f28a09fa1b24b0c3
HojinAn <asdv94@naver.com>
2022-09-28 11:23:53 +0900

    :sparkles: [FEAT] Alert state를 전역으로 관리

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,11 +1,17 @@
+import { useAppSelector } from "@/hooks";
+import Alert from "../Alert";
 import Container from "../Container";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { isAlertOn, isSuccess, message } = useAppSelector(
+    (state) => state.alert
+  );
   return (
     <Container>
       <Navbar />
+      {isAlertOn ? <Alert isSuccess={isSuccess} message={message} /> : null}
       {children}
       <Footer />
     </Container>
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -13,14 +13,36 @@ const authSlice = createSlice({
     },
   },
 });
+const alertSlice = createSlice({
+  name: "alert",
+  initialState: {
+    isAlertOn: false,
+    message: "",
+    isSuccess: false,
+  },
+  reducers: {
+    setIsAlertOn(state, action: PayloadAction<boolean>) {
+      state.isAlertOn = action.payload;
+    },
+    setAlertState(
+      state,
+      action: PayloadAction<{ message: string; isSuccess: boolean }>
+    ) {
+      state.message = action.payload.message;
+      state.isSuccess = action.payload.isSuccess;
+    },
+  },
+});
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    alert: alertSlice.reducer,
   },
 });
 
 export const { _setLogined } = authSlice.actions;
+export const { setIsAlertOn, setAlertState } = alertSlice.actions;
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
