ba03de34dd21110162ac3332435e7e815e310aaf
dltmdgh579 <dltmdgh579@gmail.com>
2022-09-30 14:57:19 +0900

    :memo: [ADD] data.sql startup IPFS file update

--- backend/uniqon/src/main/resources/data.sql
+++ backend/uniqon/src/main/resources/data.sql
@@ -23,13 +23,13 @@ values (now(), now(), "startup description", "https://uniqon-bucket.s3.ap-northe
         DATE_ADD(now(), interval 1 day), "https://ipfs.io/ipfs/bafybeihqoym3kapp5y5omzreooit2d5ngk6w26vin226xvyl644xbwu674/2.png", 0, false, false, 10, 5, null, null, "ccc", "title", 3, "nft description"),
        (now(), now(), "startup description2", "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/13e35725-8b22-4065-975e-fe15a840a1cduniqon%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8_%EC%A1%B0%EC%98%81%EB%8F%84%EB%A9%98%ED%86%A0%EB%8B%98.pdf",
         "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/fb45f1bc-0130-4932-a908-7e9f6b4b5985uniqonuniqon%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8_%EC%A1%B0%EC%98%81%EB%8F%84%EB%A9%98%ED%86%A0%EB%8B%98.jpg",null, null,
-        DATE_ADD(now(), interval 1 day), "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/22f3d64a-ddfd-49f8-900d-be43a60affe7uniqon5dc267300fefd2738de6.jpg", 0, false, false, 10, 5, null, null, "ddd", "title2", 4, "nft description2"),
+        DATE_ADD(now(), interval 1 day), "https://ipfs.io/ipfs/bafybeihqoym3kapp5y5omzreooit2d5ngk6w26vin226xvyl644xbwu674/2.png", 0, false, false, 10, 5, null, null, "ddd", "title2", 4, "nft description2"),
        (now(), now(), "startup description3", "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/13e35725-8b22-4065-975e-fe15a840a1cduniqon%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8_%EC%A1%B0%EC%98%81%EB%8F%84%EB%A9%98%ED%86%A0%EB%8B%98.pdf",
         "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/fb45f1bc-0130-4932-a908-7e9f6b4b5985uniqonuniqon%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8_%EC%A1%B0%EC%98%81%EB%8F%84%EB%A9%98%ED%86%A0%EB%8B%98.jpg",null, null,
-        DATE_ADD(now(), interval 1 day), "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/22f3d64a-ddfd-49f8-900d-be43a60affe7uniqon5dc267300fefd2738de6.jpg", 0, false, false, 10, 5, null, null, "test", "title3", 6, "nft description3"),
+        DATE_ADD(now(), interval 1 day), "https://ipfs.io/ipfs/bafybeihqoym3kapp5y5omzreooit2d5ngk6w26vin226xvyl644xbwu674/2.png", 0, false, false, 10, 5, null, null, "test", "title3", 6, "nft description3"),
        (now(), now(), "startup description4", "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/13e35725-8b22-4065-975e-fe15a840a1cduniqon%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8_%EC%A1%B0%EC%98%81%EB%8F%84%EB%A9%98%ED%86%A0%EB%8B%98.pdf",
         "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/fb45f1bc-0130-4932-a908-7e9f6b4b5985uniqonuniqon%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8_%EC%A1%B0%EC%98%81%EB%8F%84%EB%A9%98%ED%86%A0%EB%8B%98.jpg",null, null,
-        DATE_ADD(now(), interval 1 day), "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/22f3d64a-ddfd-49f8-900d-be43a60affe7uniqon5dc267300fefd2738de6.jpg", 0, false, false, 10, 5, null, null, "test", "title3", 7, "nft description4");
+        DATE_ADD(now(), interval 1 day), "https://ipfs.io/ipfs/bafybeihqoym3kapp5y5omzreooit2d5ngk6w26vin226xvyl644xbwu674/2.png", 0, false, false, 10, 5, null, null, "test", "title3", 7, "nft description4");
 
 insert into startup_question (created_date, last_modified_date, question, member_id, startup_id)
 values (now(), now(), "질문1", 1, 1),
