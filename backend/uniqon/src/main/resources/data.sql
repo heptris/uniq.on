29d95c7ca35448929714fd216d90495f5950e730
박상태 <sangtae365@naver.com>
2022-09-30 11:08:17 +0900

    :recycle: [REFACTOR] Add Dummy Data
    Add dummy data

--- backend/uniqon/src/main/resources/data.sql
+++ backend/uniqon/src/main/resources/data.sql
@@ -26,7 +26,10 @@ values (now(), now(), "startup description", "https://uniqon-bucket.s3.ap-northe
         DATE_ADD(now(), interval 1 day), "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/22f3d64a-ddfd-49f8-900d-be43a60affe7uniqon5dc267300fefd2738de6.jpg", 0, false, false, 10, 5, null, null, "ddd", "title2", 4, "nft description2"),
        (now(), now(), "startup description3", "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/13e35725-8b22-4065-975e-fe15a840a1cduniqon%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8_%EC%A1%B0%EC%98%81%EB%8F%84%EB%A9%98%ED%86%A0%EB%8B%98.pdf",
         "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/fb45f1bc-0130-4932-a908-7e9f6b4b5985uniqonuniqon%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8_%EC%A1%B0%EC%98%81%EB%8F%84%EB%A9%98%ED%86%A0%EB%8B%98.jpg",null, null,
-        DATE_ADD(now(), interval 1 day), "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/22f3d64a-ddfd-49f8-900d-be43a60affe7uniqon5dc267300fefd2738de6.jpg", 0, false, false, 10, 5, null, null, "test", "title3", 6, "nft description3");
+        DATE_ADD(now(), interval 1 day), "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/22f3d64a-ddfd-49f8-900d-be43a60affe7uniqon5dc267300fefd2738de6.jpg", 0, false, false, 10, 5, null, null, "test", "title3", 6, "nft description3"),
+       (now(), now(), "startup description4", "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/13e35725-8b22-4065-975e-fe15a840a1cduniqon%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8_%EC%A1%B0%EC%98%81%EB%8F%84%EB%A9%98%ED%86%A0%EB%8B%98.pdf",
+        "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/fb45f1bc-0130-4932-a908-7e9f6b4b5985uniqonuniqon%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8_%EC%A1%B0%EC%98%81%EB%8F%84%EB%A9%98%ED%86%A0%EB%8B%98.jpg",null, null,
+        DATE_ADD(now(), interval 1 day), "https://uniqon-bucket.s3.ap-northeast-2.amazonaws.com/startup/22f3d64a-ddfd-49f8-900d-be43a60affe7uniqon5dc267300fefd2738de6.jpg", 0, false, false, 10, 5, null, null, "test", "title3", 7, "nft description4");
 
 insert into startup_question (created_date, last_modified_date, question, member_id, startup_id)
 values (now(), now(), "질문1", 1, 1),
@@ -60,7 +63,10 @@ values (now(), now(), "INVESTING", 1, 1),
        (now(), now(), "INVESTING", 1, 2),
        (now(), now(), "INVESTING", 2, 2),
        (now(), now(), "INVESTED", 6, 1),
-       (now(), now(), "INVESTING", 6, 2);
+       (now(), now(), "INVESTING", 6, 2),
+       (now(), now(), "INVESTED", 7, 1),
+       (now(), now(), "INVESTING", 7, 2),
+       (now(), now(), "CANCELED", 7, 3);
 --        (now(), now(), "INVESTING", 6, 3);
 
 insert into alarm  (created_date, last_modified_date, content, is_read, member_id)
@@ -75,12 +81,19 @@ values (now(), now(), "예약한 펀딩이 성공했습니다 !!", false, 1),
        (now(), now(), "예약한 펀딩이 실패했습니다 !!", false, 5),
        (now(), now(), "예약한 펀딩이 성공했습니다 !!", false, 6),
        (now(), now(), "SSF를 입금하시겠습니까 ?", true, 6),
-       (now(), now(), "입금이 완료되었습니다 !!", false, 6);
+       (now(), now(), "입금이 완료되었습니다 !!", false, 6),
+       (now(), now(), "예약한 펀딩이 실패했습니다 !!", false, 7),
+       (now(), now(), "예약한 펀딩이 성공했습니다 !!", false, 7),
+       (now(), now(), "SSF를 입금하시겠습니까 ?", true, 7),
+       (now(), now(), "입금이 완료되었습니다 !!", false, 7);
 
 insert into startup_favorite (created_date, last_modified_date, is_fav, member_id, startup_id)
 values (now(), now(), true, 1, 1),
        (now(), now(), false, 1, 2),
        (now(), now(), false, 6, 1),
-       (now(), now(), true, 6, 2);
+       (now(), now(), true, 6, 2),
+       (now(), now(), false, 7, 1),
+       (now(), now(), true, 7, 2),
+       (now(), now(), true, 7, 3);
 
 
