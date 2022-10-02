751f3a791b4c3b39bad011422b23166c8fe98101
신은정 <ejshin0213@naver.com>
2022-10-03 01:25:07 +0900

    :sparkles: [FEAT] RSRVItemCard 컴포넌트 생성

---
+++ frontend/src/components/Card/RSRVItemCard.tsx
@@ -0,0 +1,151 @@
+import { ElementType, forwardRef, Ref } from "react";
+import Image, { StaticImageData } from "next/image";
+
+import { css, useTheme } from "@emotion/react";
+import styled from "@emotion/styled";
+import { faEthereum } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import type { Combine } from "@/types/utils";
+import type { CardProps } from "@/types/props";
+import type { RSRVItem } from "@/types/api_responses";
+import Card from "@/components/Card";
+import Text from "@/components/Text";
+import ProgressBar from "@/components/ProgressBar";
+
+type RSRVItemCardProps =
+  | {
+      nftReserveCount?: number;
+    }
+  | RSRVItem;
+
+/**
+ * @params
+ * `nftImage`: `string` | `StaticImageData`
+ *
+ * `tokenId`: `string`
+ *
+ * `startupName`: `string`
+ *
+ * `price`: `number`
+ *
+ * `status`: `string`
+ *
+ * `progress`: `number`
+ * @returns `ReactElement`
+ */
+function RSRVItemCard<T extends ElementType = "div">(
+  props: Combine<RSRVItemCardProps, CardProps<T>>,
+  ref: Ref<any>
+) {
+  const {
+    tokenId,
+    startupId,
+    nftImage,
+    startupName,
+    nftPrice,
+    nftReserveCount,
+    ...rest
+  } = props;
+
+  const theme = useTheme();
+
+  return (
+    <Card style={{ height: "fit-content" }} ref={ref} {...rest}>
+      <ImageContainer>
+        <Image
+          src={nftImage}
+          layout={"fill"}
+          objectFit={"cover"}
+          css={css`
+            z-index: -2;
+          `}
+        />
+      </ImageContainer>
+      <InfoContainer>
+        <Text
+          role="token-name"
+          css={css`
+            font-weight: 600;
+          `}
+        >
+          {startupName} #{tokenId}
+        </Text>
+        <Text
+          role="corp-name"
+          css={css`
+            color: ${theme.color.text.sub};
+            font-size: 0.7rem;
+            margin-bottom: 0.5rem;
+          `}
+        >
+          {startupName}
+        </Text>
+        <Text
+          as="div"
+          role="price"
+          css={css`
+            display: flex;
+            align-items: center;
+            font-weight: 500;
+            font-size: 0.8rem;
+            margin-bottom: 0.5rem;
+          `}
+        >
+          <FontAwesomeIcon
+            css={css`
+              width: 0.6rem;
+              margin-right: 0.5rem;
+            `}
+            icon={faEthereum}
+          />
+          {nftPrice} ETH
+        </Text>
+        <Text
+          role="status"
+          css={css`
+            font-size: 0.8rem;
+            display: flex;
+            justify-content: start;
+            align-items: center;
+          `}
+        >
+          {nftReserveCount !== undefined && (
+            <>
+              <ProgressBar
+                css={css`
+                  margin-right: 5px;
+                `}
+                progress={nftReserveCount}
+                type={"blue"}
+              />
+              <Text
+                css={css`
+                  font-size: 0.7rem;
+                  font-weight: 600;
+                `}
+              >
+                {nftReserveCount}%
+              </Text>
+            </>
+          )}
+        </Text>
+      </InfoContainer>
+    </Card>
+  );
+}
+
+const InfoContainer = styled.div`
+  padding: 0.5rem 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const ImageContainer = styled.div`
+  position: relative;
+  display: block;
+  aspect-ratio: 1 / 1;
+  overflow: hidden;
+`;
+
+export default forwardRef(RSRVItemCard) as typeof RSRVItemCard;
