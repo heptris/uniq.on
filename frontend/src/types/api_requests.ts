1435a6930f0f9a1c1f661208503ded4162c768a0
HojinAn <asdv94@naver.com>
2022-10-04 01:19:59 +0900

    :label: [ADD] api request용 ApplyForm 타입 추가

--- frontend/src/types/api_requests.ts
+++ frontend/src/types/api_requests.ts
@@ -1,6 +1,19 @@
 import { Member, NFTItem } from "./api_responses";
 
-export type SignupForm = Omit<Member, "id" | "profileImage" | "memberType">;
+export type SignupFormType = Omit<Member, "id" | "profileImage" | "memberType">;
 export type StoreNFTParams = Omit<NFTItem, "nftImage" | "tokenId"> & {
   nftImage: Blob | File;
+};
+export type ApplyFormType = {
+  title: string;
+  description: string;
+  dueDate: string;
+  discordUrl: string;
+  businessPlanFile: any;
+  roadMapFile: any;
+  nftTargetCount: number;
+  nftPrice: number;
+  nftDescription: string;
+  nftImageFile: any;
+  isChecked: boolean;
 };
