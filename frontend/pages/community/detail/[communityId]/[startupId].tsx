bcdf427ac06f929c756973f15f5e1c7579a92569
신은정 <ejshin0213@naver.com>
2022-10-06 03:26:19 +0900

    :sparkles: [FEAT] 커뮤니티 게시글 detail 페이지 생성

---
+++ frontend/pages/community/detail/[communityId]/[startupId].tsx
@@ -0,0 +1,371 @@
+import { ENDPOINT_API } from "@/api/endpoints";
+import Button from "@/components/Button";
+import Card from "@/components/Card";
+import Grid from "@/components/Grid";
+import LabelInput from "@/components/LabelInput";
+import Text from "@/components/Text";
+import { useAlert } from "@/hooks";
+import { minDesktopWidth } from "@/styles/utils";
+import { css, useTheme } from "@emotion/react";
+import styled from "@emotion/styled";
+import { faCommentDots, faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { communityId, startupId } = context.query;
+  const CommentRequest = await axios
+    .get(`${ENDPOINT_API}/invest/community/detail/${communityId}`)
+    .then(({ data }) => data.data);
+  const ProfileRequest = await axios
+    .get(`${ENDPOINT_API}/member`)
+    .then(({ data }) => data.data);
+  return {
+    props: {
+      CommentRequest,
+      communityId,
+      startupId,
+      ProfileRequest,
+    },
+  };
+};
+
+export type CommentListProps = {
+  children?: CommentListProps[];
+  commentId: number;
+  content: string;
+  myComment: boolean;
+  nickName: string;
+  parentId: number;
+  updateDate: string;
+};
+
+type ProfileProps = {
+  walletAddress: string;
+  nickName: string;
+  email: string;
+  profileImage: string;
+  memberType: string;
+};
+type CommunityDetailProps = {
+  commentList: CommentListProps[];
+  content: string;
+  createdDate: string;
+  hit: number;
+  nickName: string;
+  title: string;
+};
+
+type DetailProps = {
+  CommentRequest: CommunityDetailProps;
+  ProfileRequest: ProfileProps;
+  communityId: number;
+  startupId: number;
+};
+
+export default function detail(props: DetailProps) {
+  const router = useRouter();
+  const theme = useTheme();
+  const { handleAlertOpen } = useAlert();
+  const { CommentRequest, ProfileRequest, communityId, startupId } = props;
+  const [content, setContent] = useState("");
+  console.log(CommentRequest);
+  console.log(ProfileRequest);
+  const onDeleteHandler = async () => {
+    await axios
+      .delete(`${ENDPOINT_API}/invest/community/${startupId}/${communityId}`)
+      .then((res) => {
+        console.log(res);
+        handleAlertOpen(2000, "게시글 삭제가 완료되었습니다", true);
+        // router.push(`/community/${startupId}`); //startupName 필요
+      })
+      .catch((err) => {
+        console.log(err);
+        handleAlertOpen(2000, "게시글 삭제가 실패했습니다", false);
+      });
+  };
+  const onSubmitComment = () => {
+    const data = {
+      //parentId는 어떤거 보내야하는지 확인
+      content,
+    };
+    axios
+      .post(`${ENDPOINT_API}/invest/community/${communityId}/content`, data)
+      .then((res) => {
+        console.log(res);
+        handleAlertOpen(2000, "댓글이 등록되었습니다", true);
+      })
+      .catch((err) => {
+        console.log(err);
+        handleAlertOpen(2000, "댓글 등록이 실패했습니다.", false);
+      });
+  };
+
+  return (
+    <Grid
+      css={css`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 100%;
+        margin-top: 3rem;
+
+        @media (${minDesktopWidth}) {
+          width: 70%;
+          grid-template-columns: repeat(2, 1fr);
+        }
+      `}
+    >
+      <Card
+        css={css`
+          margin-top: 0.5rem;
+        `}
+      >
+        <CardContent>
+          <div
+            css={css`
+              display: flex;
+              flex-direction: column;
+              justify-content: space-between;
+            `}
+          >
+            <Text
+              css={css`
+                align-self: flex-start;
+                font-size: 2rem;
+                font-weight: 600;
+                margin-bottom: 1rem;
+              `}
+            >
+              {CommentRequest.title}
+            </Text>
+            <div
+              css={css`
+                display: flex;
+                width: 100%;
+                justify-content: space-between;
+                align-items: center;
+              `}
+            >
+              <div
+                css={css`
+                  display: flex;
+                  flex-direction: column;
+                `}
+              >
+                <Text
+                  as="p"
+                  css={css`
+                    font-size: 1.5rem;
+                  `}
+                >
+                  {CommentRequest.nickName}
+                </Text>
+                <Text
+                  as="h2"
+                  css={css`
+                    font-size: 0.8rem;
+                  `}
+                >
+                  {CommentRequest.createdDate.substring(0, 10)}
+                </Text>
+              </div>
+              <Text
+                css={css`
+                  display: flex;
+                  align-items: center;
+                  color: ${theme.color.text.sub};
+                `}
+              >
+                <FontAwesomeIcon
+                  icon={faEye}
+                  css={css`
+                    width: 1rem;
+                    color: ${theme.color.text.sub};
+                    margin-right: 0.5rem;
+                  `}
+                />
+                조회수 {CommentRequest.hit}
+              </Text>
+            </div>
+          </div>
+          <hr
+            css={css`
+              width: 100%;
+              color: ${theme.color.text.main};
+              margin: 2rem 0;
+            `}
+          />
+          <Text>{CommentRequest.content}</Text>
+        </CardContent>
+      </Card>
+      {ProfileRequest.nickName === CommentRequest.nickName && (
+        <div
+          css={css`
+            align-self: flex-end;
+          `}
+        >
+          <Link href={`/community/update/${startupId}/${communityId}`}>
+            <Button
+              css={css`
+                margin-right: 1rem;
+              `}
+            >
+              수정
+            </Button>
+          </Link>
+
+          <Button onClick={onDeleteHandler}>삭제</Button>
+        </div>
+      )}
+      <CommentWriteWrapper>
+        <Text
+          as="h1"
+          css={css`
+            display: flex;
+            align-items: center;
+            margin-bottom: 1rem;
+            color: ${theme.color.text.sub};
+          `}
+        >
+          <FontAwesomeIcon
+            icon={faCommentDots}
+            css={css`
+              width: 1rem;
+              color: ${theme.color.text.sub};
+              margin-right: 0.5rem;
+            `}
+          />
+          댓글 {CommentRequest.commentList.length}
+        </Text>
+        <LabelInput
+          css={css`
+            width: 100%;
+            height: 10rem;
+            margin-bottom: 1rem;
+          `}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setContent(e.target.value)
+          }
+        />
+        <Button
+          onClick={onSubmitComment}
+          css={css`
+            align-self: flex-end;
+          `}
+        >
+          댓글등록
+        </Button>
+      </CommentWriteWrapper>
+      <hr
+        css={css`
+          color: ${theme.color.text.hover};
+          width: 100%;
+        `}
+      />
+      {CommentRequest.commentList.map((comment, i) => (
+        <>
+          <div
+            css={css`
+              width: 100%;
+              display: flex;
+              flex-direction: row;
+              justify-content: space-between;
+              align-items: center;
+              padding: 0 2rem;
+              box-sizing: border-box;
+            `}
+            key={i}
+          >
+            <div
+              css={css`
+                display: flex;
+                flex-direction: column;
+              `}
+            >
+              <Text
+                as="h1"
+                css={css`
+                  font-size: 1.5rem;
+                `}
+              >
+                {comment.nickName}
+              </Text>
+              <Text
+                as="p"
+                css={css`
+                  font-size: 0.8rem;
+                  color: ${theme.color.text.hover};
+                `}
+              >
+                {comment.updateDate.substring(0, 10)}
+              </Text>
+            </div>
+
+            <Text>{comment.content}</Text>
+            {comment.nickName !== ProfileRequest.nickName ? (
+              <Text
+                css={css`
+                  color: ${theme.color.text.sub};
+                  &:hover {
+                    color: ${theme.color.text.hover};
+                    cursor: pointer;
+                  }
+                `}
+              >
+                댓글작성
+              </Text>
+            ) : (
+              <>
+                <Button
+                  css={css`
+                    margin-right: 1rem;
+                  `}
+                >
+                  수정
+                </Button>
+                <Button>삭제</Button>
+              </>
+            )}
+          </div>
+          <hr
+            css={css`
+              color: ${theme.color.text.hover};
+              width: 100%;
+            `}
+          />
+        </>
+      ))}
+    </Grid>
+  );
+}
+const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+
+  h1 {
+    font-size: 1.5rem;
+    font-weight: 700;
+    padding: 1rem 0;
+  }
+  h2 {
+    display: inline;
+    color: ${({ theme }) => theme.color.text.sub};
+    font-weight: 600;
+  }
+  p {
+    font-weight: 500;
+  }
+`;
+
+const CommentWriteWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  padding-top: 3rem;
+`;
