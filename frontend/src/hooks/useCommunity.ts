0e33afc0c5f7e34e31f9e822698e5ee27173d5a2
신은정 <ejshin0213@naver.com>
2022-10-07 01:59:07 +0900

    :sparkles: [FEAT] useCommunity hook 생성

---
+++ frontend/src/hooks/useCommunity.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+import { ENDPOINT_API } from "@/api/endpoints";
+import { QUERY_KEYS } from "@/api/query_key_schema";
+
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { CommunityDetailProps } from "@/pages/community/detail/[communityId]/[startupId]";
+
+const { MY_COMMUNITY_LIST } = QUERY_KEYS;
+
+const getCommunityList = async (communityId: number) =>
+  await axios
+    .get(`${ENDPOINT_API}/invest/community/detail/${communityId}`)
+    .then(({ data }) => data.data);
+
+const useCommunity = ({
+  communityList,
+  communityId,
+}: {
+  communityList: CommunityDetailProps;
+  communityId: number;
+}) =>
+  useQuery([MY_COMMUNITY_LIST], () => getCommunityList(communityId), {
+    initialData: communityList,
+  });
+
+const commentDeleteMutation = () =>
+  useMutation(
+    ({ communityId, commentId }: { commentId: number; communityId: number }) =>
+      axios.delete(
+        `${ENDPOINT_API}/invest/community-comments/${communityId}/${commentId}`
+      )
+  );
+const commentCreateMutation = () =>
+  useMutation(({ communityId, data }: { communityId: number; data: any }) =>
+    axios.post(
+      `${ENDPOINT_API}/invest/community-comments/${communityId}/comment`,
+      data
+    )
+  );
+export {
+  useCommunity,
+  getCommunityList,
+  commentDeleteMutation,
+  commentCreateMutation,
+};
