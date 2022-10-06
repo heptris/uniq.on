39dc07e65f1a78ed90ee6de48415297ad307c4cd
전한울 <wjsgksdnf11@naver.com>
2022-10-06 23:22:06 +0900

    :recycle: [REFACTOR] StartupService
    [REFACTOR] StartupService

--- backend/uniqon/src/main/java/com/ssafy/uniqon/service/startup/StartupService.java
+++ backend/uniqon/src/main/java/com/ssafy/uniqon/service/startup/StartupService.java
@@ -137,14 +137,18 @@ public class StartupService {
         }
 
         if (road_map != null) {
-            try {
-                awsS3 = awsS3Service.upload(road_map, "startup");
-            }catch (IOException e){
-                throw new CustomException(FILE_UPLOAD_ERROR);
+//            try {
+//                awsS3 = awsS3Service.upload(road_map, "startup");
+//            }catch (IOException e){
+//                throw new CustomException(FILE_UPLOAD_ERROR);
+//            }
+//
+//            String roadMapUrl = awsS3.getPath();
+//            savedStartup.changeRoadMap(roadMapUrl);
+            if("application/pdf".equals(road_map.getContentType())) {
+                String imgUrl = awsS3Service.pdfToImg(road_map);
+                savedStartup.changeRoadMap(imgUrl);
             }
-
-            String roadMapUrl = awsS3.getPath();
-            savedStartup.changeRoadMap(roadMapUrl);
         }
         return savedStartup.getId();
     }
